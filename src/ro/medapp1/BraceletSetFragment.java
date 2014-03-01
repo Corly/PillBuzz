a132606077a325037ac462881d7a4e46c2ef08f7
ancadiana23 <ancadianavicol@gmail.com>
2014-03-01 16:40:09 +0200

    Setmin/max value

--- src/ro/medapp1/BraceletSetFragment.java
+++ src/ro/medapp1/BraceletSetFragment.java
@@ -25,18 +25,49 @@ public class BraceletSetFragment extends Fragment {
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-//		final NumberPicker delay=(NumberPicker)getView().findViewById(R.id.delay_number_picker);
-//		final NumberPicker bufferNumber=(NumberPicker)getView().findViewById(R.id.buffer_number_picker);
-//		final NumberPicker bufferUnit=(NumberPicker)getView().findViewById(R.id.buffer_unit_number_picker);
-//		
-//		delay.setMaxValue(60);
-//		delay.setMinValue(0);
+
 	
 	}
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
         Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+  
+		View v=inflater.inflate(R.layout.fragment_bracelet_settings, container, false);
+
+		final NumberPicker delay=(NumberPicker)v.findViewById(R.id.delay_number_picker);
+//		final NumberPicker bufferNumber=(NumberPicker)v.findViewById(R.id.buffer_number_picker);
+//		final NumberPicker bufferUnit=(NumberPicker)v.findViewById(R.id.buffer_unit_number_picker);
+		
+		delay.setMaxValue(60);
+		delay.setMinValue(0);
+//		
+//		bufferUnit.setMinValue(0);
+//		bufferUnit.setMaxValue(2);
+//		
+//		bufferUnit.setDisplayedValues( new String[] { "hours", "days", "weeks" });
+		
+		
+//		bufferUnit.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+//			
+//			@Override
+//			public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+//				switch(newVal)
+//				{
+//				case 0: {
+//					bufferNumber.setMaxValue(24);
+//					bufferNumber.setMinValue(1);				
+//				}break;
+//				case 1: {
+//					bufferNumber.setMaxValue(30);
+//					bufferNumber.setMinValue(1);
+//				}break;
+//				case 2: {
+//					bufferNumber.setMaxValue(2);
+//					bufferNumber.setMinValue(1);
+//				}break;
+//				}
+//			}});
+//			
         return inflater.inflate(R.layout.fragment_bracelet_settings, container, false);
     }
 
