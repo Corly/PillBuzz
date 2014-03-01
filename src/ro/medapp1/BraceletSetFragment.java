5e22cd043101f2cb0fd20a632ee23d59d6393029
ancadiana23 <ancadianavicol@gmail.com>
2014-03-01 16:47:54 +0200

    Setmin/max value fara comm

--- src/ro/medapp1/BraceletSetFragment.java
+++ src/ro/medapp1/BraceletSetFragment.java
@@ -35,41 +35,41 @@ public class BraceletSetFragment extends Fragment {
 		View v=inflater.inflate(R.layout.fragment_bracelet_settings, container, false);
 
 		final NumberPicker delay=(NumberPicker)v.findViewById(R.id.delay_number_picker);
-//		final NumberPicker bufferNumber=(NumberPicker)v.findViewById(R.id.buffer_number_picker);
-//		final NumberPicker bufferUnit=(NumberPicker)v.findViewById(R.id.buffer_unit_number_picker);
+		final NumberPicker bufferNumber=(NumberPicker)v.findViewById(R.id.buffer_number_picker);
+		final NumberPicker bufferUnit=(NumberPicker)v.findViewById(R.id.buffer_unit_number_picker);
 		
 		delay.setMaxValue(60);
 		delay.setMinValue(0);
-//		
-//		bufferUnit.setMinValue(0);
-//		bufferUnit.setMaxValue(2);
-//		
-//		bufferUnit.setDisplayedValues( new String[] { "hours", "days", "weeks" });
+		
+		bufferUnit.setMinValue(0);
+		bufferUnit.setMaxValue(2);
+		
+		bufferUnit.setDisplayedValues( new String[] { "hours", "days", "weeks" });
 		
 		
-//		bufferUnit.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
-//			
-//			@Override
-//			public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
-//				switch(newVal)
-//				{
-//				case 0: {
-//					bufferNumber.setMaxValue(24);
-//					bufferNumber.setMinValue(1);				
-//				}break;
-//				case 1: {
-//					bufferNumber.setMaxValue(30);
-//					bufferNumber.setMinValue(1);
-//				}break;
-//				case 2: {
-//					bufferNumber.setMaxValue(2);
-//					bufferNumber.setMinValue(1);
-//				}break;
-//				}
-//			}});
-//			
-        return inflater.inflate(R.layout.fragment_bracelet_settings, container, false);
-    }
+		bufferUnit.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+			
+			@Override
+			public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+				switch(newVal)
+				{
+				case 0: {
+					bufferNumber.setMaxValue(24);
+					bufferNumber.setMinValue(1);				
+				}break;
+				case 1: {
+					bufferNumber.setMaxValue(30);
+					bufferNumber.setMinValue(1);
+				}break;
+				case 2: {
+					bufferNumber.setMaxValue(2);
+					bufferNumber.setMinValue(1);
+				}break;
+				}
+			}});
+			
+			return v; 
+			}
 
 		
 }
