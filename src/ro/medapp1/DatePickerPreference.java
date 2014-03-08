6900a7f318f8b31c4a393a5a100e6d8be96983ae
Dragos Corlatescu <dragos.corlatescu@gmail.com>
2014-03-09 01:48:17 +0200

    Add functionality on the save button.

--- src/ro/medapp1/DatePickerPreference.java
+++ src/ro/medapp1/DatePickerPreference.java
@@ -3,10 +3,13 @@ package ro.medapp1;
 import android.content.Context;
 import android.preference.DialogPreference;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.DatePicker;
 
 // clasa asta deocmdata doar ofera interfata insa nu pastreaza informatia
 //trebuie sa implementam noi metodele in functie de cum o sa o pastram
 public class DatePickerPreference extends DialogPreference {
+	private DatePicker picker;
 
 	public DatePickerPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -17,12 +20,40 @@ public class DatePickerPreference extends DialogPreference {
         
         setDialogIcon(null);
     }
+	
+	@Override
+	protected View onCreateDialogView() {
+		picker = new DatePicker(getContext());
+		return picker;
+	}
+	
 	@Override
 	protected void onDialogClosed(boolean positiveResult) {
 	    // When the user selects "OK", persist the new value
-//	    if (positiveResult) {
-//	        persistInt(mNewValue);
-//	    }
+		
+	    if (positiveResult) {
+	    	int day = picker.getDayOfMonth();
+			int month = picker.getMonth();
+			int year = picker.getYear();
+			
+			String date = "";
+			if (day < 10) {
+				date += "0" + day + "/";
+			} else {
+				date += day + "/";
+			}
+			
+			if (month < 10) {
+				date += "0" + month + "/";
+			} else {
+				date += month + "/";
+			}
+			
+			date += "" + year;
+			
+	    	persistString(date);
+	    	setSummary(date);
+	    }
 	}
 	
 	@Override
