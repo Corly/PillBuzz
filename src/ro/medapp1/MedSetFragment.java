d39d4651fc53a22da0fe1bcd4d70cc8d84f75878
Dragos Corlatescu <dragos.corlatescu@gmail.com>
2014-05-11 15:57:22 +0300

    Add posibility of deleting meds on long click.

--- src/ro/medapp1/MedSetFragment.java
+++ src/ro/medapp1/MedSetFragment.java
@@ -1,24 +1,25 @@
 package ro.medapp1;
 
-
-import java.net.MalformedURLException;
 import java.util.ArrayList;
 
-import android.app.Activity;
+import ro.pillbuzz.data.MedDb;
+
+import android.app.AlertDialog;
 import android.app.ListFragment;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.ProgressBar;
 
 
-
-
-public class MedSetFragment extends ListFragment{
+public class MedSetFragment extends ListFragment {
 	
-	ArrayAdapter adapter;
+	ArrayAdapter<String> adapter;
 	ArrayList<String> medNames = new ArrayList<String>();
 	//static Activity context;
 	
@@ -39,24 +40,59 @@ public class MedSetFragment extends ListFragment{
 	
 	@Override
 	public void setArguments(Bundle args) {
-		// TODO Auto-generated method stub
 		super.setArguments(args);
 	}
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
-		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
-	
-	
 		
-		if(getActivity()!=null)
-		
-		{
-		adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, medNames);
-		setListAdapter(adapter);
-
+		if(getActivity()!=null) {
+			adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, medNames);
+			setListAdapter(adapter);
 		}
+	}
+	
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
+		
+		getListView().setOnItemLongClickListener(new OnItemLongClickListener() {
+
+			@Override
+			public boolean onItemLongClick(AdapterView<?> parent, View view,
+					int position, long id) {
+				if (position != MedVector.getInstance().getList().size()) {
+					final int pos = position;
+					AlertDialog.Builder dialog = new AlertDialog.Builder(getActivity());
+					dialog.setTitle("Delete medicine");
+					dialog.setMessage("Are you sure you want to delete this medicine?");
+					dialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+
+						@Override
+						public void onClick(DialogInterface d, int which) {
+							MedDb db = new MedDb(getActivity());
+							db.deleteMed(MedVector.getInstance().getList().get(pos));
+							MedVector.getInstance().removeMedFromList(getActivity(), 
+									MedVector.getInstance().getList().get(pos));
+							medNames.remove(pos);
+							adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, medNames);
+							setListAdapter(adapter);
+						}
+					});
+					dialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+						
+						@Override
+						public void onClick(DialogInterface d, int which) {
+							d.dismiss();
+						}
+					});
+					dialog.create().show();
+				}
+				return false;
+			}
+		});
+
 	}
 	
 	@Override
@@ -70,15 +106,11 @@ public class MedSetFragment extends ListFragment{
 		medNames.add("+");
 		adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, medNames);
 		setListAdapter(adapter);
-		//adapter.notifyDataSetChanged();
-		//adapter.notifyDataSetInvalidated();
-		//setListAdapter(new ArrayAdapter<String>(context, android.R.layout.simple_list_item_1, medNames));
 	}
 
 
 	@Override
 	public void onListItemClick(ListView l, View v, int position, long id) {
-		// TODO Auto-generated method stub
 		super.onListItemClick(l, v, position, id);
 		
 		Class medClass=null;
@@ -88,13 +120,8 @@ public class MedSetFragment extends ListFragment{
 			medSet.putExtra("position", position);
 			startActivity(medSet);
 		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
-		
-		
-		
+		}	
 	}
-	
 
 }
